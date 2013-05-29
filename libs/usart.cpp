b3326eb900082618223431e530823e47af95b296
Kamil <kamil@freakone.pl>
2013-05-29 12:22:25 +0200

    ustawianie adresu urządzenia

--- libs/usart.cpp
+++ libs/usart.cpp
@@ -1,6 +1,7 @@
 #include "usart.h"
 #include "motor.h"
 #include "globals.h"
+#include <avr/eeprom.h>
 
 static unsigned char buffer[30];
 static unsigned int pos = 0;
@@ -28,33 +29,47 @@ ISR(USART_RX_vect)
 	pos++;
 	
 
-	if(UDR0 == 0x0A &&  buffer[0] == 0xff && (buffer[1] == ADDRESS || buffer[1] == 0x10))//sprawdzamy czy została przesłąna poprawna ramka	
+	if(UDR0 == 0x0A &&  buffer[0] == 0xff)//sprawdzamy czy została przesłąna poprawna ramka	
 	{	
-		switch(buffer[2])
+		if(buffer[1] == ADDRESS || buffer[1] == 0x10) // komendy jednostkowe
 		{
-			case 0x70:
+		
+			switch(buffer[2])
+			{
+				case 0x70:
+				
+				uart_put(0xff);
+				uart_put(buffer[1]);
+				uart_put(0x70);
+				dec2hascii(ADDRESS, 3);
+				uart_put('\n');
+				break;
+				
+				case 0x71:
+				for(int i = 3; i < 6; i++)
+				{					
+					hascii3[i - 3] = buffer[i];
+				}
+				ADDRESS = hascii2dec(hascii3);
+				eeprom_write_word((uint16_t*)1, (uint16_t)ADDRESS);
+				
+				uart_put(0xff);
+				uart_put(ADDRESS);
+				uart_put(0x70);
+				dec2hascii(ADDRESS, 3);
+				uart_put('\n');
 			
-			uart_put(0xff);
-			uart_put(buffer[1]);
-			uart_put(0x70);
-			dec2hascii(ADDRESS, 3);
-			uart_put('\n');
-			break;
-			
-			case 0x71:
-			
-			hascii3[0] = '0';
-			hascii3[1] = '0';
-			hascii3[2] = 'B';
-			for(int i = 3; i < 6; i++)
-			{					
-				hascii3[i - 3] = buffer[i];
 			}
-			uart_put(hascii2dec(hascii3));
-	
+			
+		}
+		else if(buffer[1] == 0x10) // komendy dla wszystkich
+		{
+		
 		
 		}
 		
+		
+			
 		pos = 0;
 	
 	}
